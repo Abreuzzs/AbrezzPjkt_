afaad4ca94be2388edf5b273668fb190b00aab8c
utziacre <utziacre@gmail.com>
2024-01-27 14:01:54 +0800

    PM / EM: Adjust the frequency used to calculate energy

--- include/linux/energy_model.h
+++ include/linux/energy_model.h
@@ -8,6 +8,7 @@
 #include <linux/sched/cpufreq.h>
 #include <linux/sched/topology.h>
 #include <linux/types.h>
+#include <linux/cpufreq.h>
 
 #ifdef CONFIG_ENERGY_MODEL
 
@@ -110,7 +111,7 @@ int em_register_perf_domain(cpumask_t *span, unsigned int nr_states,
 static inline unsigned long em_pd_energy(struct em_perf_domain *pd,
 				unsigned long max_util, unsigned long sum_util)
 {
-	unsigned long freq, scale_cpu;
+	unsigned long freq, min_freq, scale_cpu;
 	struct em_cap_state *cs;
 	int i, cpu;
 #ifdef CONFIG_NONLINEAR_FREQ_CTL
@@ -129,7 +130,11 @@ static inline unsigned long em_pd_energy(struct em_perf_domain *pd,
 	freq = mtk_map_util_freq(cpu, max_util);
 #else
 	freq = map_util_freq(max_util, cs->frequency, scale_cpu);
+        min_freq = cpufreq_quick_get_min(cpu);
+
+        freq = (freq > min_freq) ? freq : min_freq;
 #endif
+
 	/*
 	 * Find the lowest capacity state of the Energy Model above the
 	 * requested frequency.
