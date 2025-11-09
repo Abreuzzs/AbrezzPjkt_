7f184db93972ace8127b2fedf8072821e2b595b1
AbreuzzReserva <githubb01u@gmail.com>
2025-11-09 18:12:24 +0000

    asm:jump_label.h Align with https://github.com/user-why-red/android_kernel_xiaomi_sdm660_419

--- arch/arm64/include/asm/jump_label.h
+++ arch/arm64/include/asm/jump_label.h
@@ -26,8 +26,8 @@
 
 #define JUMP_LABEL_NOP_SIZE		AARCH64_INSN_SIZE
 
-static __always_inline bool arch_static_branch(struct static_key *key,
-					       bool branch)
+static __always_inline bool arch_static_branch(struct static_key * const key,
+					       const bool branch)
 
 {
 	asm_volatile_goto(
@@ -44,8 +44,8 @@ l_yes:
 	return true;
 }
 
-static __always_inline bool arch_static_branch_jump(struct static_key *key,
-						    bool branch)
+static __always_inline bool arch_static_branch_jump(struct static_key * const key,
+						    const bool branch)
 
 {
 	asm_volatile_goto(
