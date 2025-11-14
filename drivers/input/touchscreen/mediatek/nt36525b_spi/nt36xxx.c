0e4810f0f1c6fc1a9561a8ca5466fac7f7594e77
Tauan Abrel <tauanabrelcelularnovo@gmail.com>
2025-11-14 03:20:50 +0000

    Revert "touchscreen: NT36xxx_SPI: Add a delay in SPI functions"
    This reverts commit 4c9878775bb87367d782509abb40871ad8605c1f.

--- drivers/input/touchscreen/mediatek/nt36525b_spi/nt36xxx.c
+++ drivers/input/touchscreen/mediatek/nt36525b_spi/nt36xxx.c
@@ -275,7 +275,6 @@ int32_t CTP_SPI_READ(struct spi_device *client, uint8_t *buf, uint16_t len)
 		ret = spi_read_write(client, buf, len, NVTREAD);
 		if (ret == 0) break;
 		retries++;
-		msleep(20);
 	}
 
 	if (unlikely(retries == 5)) {
@@ -310,7 +309,6 @@ int32_t CTP_SPI_WRITE(struct spi_device *client, uint8_t *buf, uint16_t len)
 		ret = spi_read_write(client, buf, len, NVTWRITE);
 		if (ret == 0)	break;
 		retries++;
-		msleep(20);
 	}
 
 	if (unlikely(retries == 5)) {
